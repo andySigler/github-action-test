76a9a6d9e7f4b28779878002d15c04db11766a31
andySigler <andrewsigler1@gmail.com>
2022-03-30 07:21:49 -0400

    cleans build script up a bit

--- github_action_test/production_tests/script_a/main.py
+++ github_action_test/production_tests/script_a/main.py
@@ -2,15 +2,15 @@ import time
 
 # NOTE: we can NO LONGER "insert" paths to scripts
 #       this would prevent us from being able to "freeze" into an EXE
-from github_action_test.utils.version import generate_script_version
+from github_action_test.utils.version import generate_script_version as test_version
 from github_action_test.drivers.example_driver import example_driver_method
 
 from github_action_test.production_tests.script_a.helper import helper_method
 
 
 if __name__ == '__main__':
-    # script version must be stored in the ".version" file next to __init__.py
-    print('Script version:', generate_script_version(__file__))
+    # version is stored in ".version" file (next to main.py)
+    print('Test version:', test_version(__file__))
     while True:
         time.sleep(1)
         print(helper_method())
--- github_action_test/utils/build.py
+++ github_action_test/utils/build.py
@@ -10,9 +10,8 @@ from .version import read_version_file, get_git_commit_hash, add_commit_hash_to_
 from .version import VERSION_FILE_NAME
 
 
-RELATIVE_BUILD_DIR = './build'
-RELATIVE_SCRIPTS_DIR = '../production_tests'
-TEMP_PY_NAME = 'tmp.py'  # the actual script PyInstaller builds
+DIST_DIR = 'dist'
+TARGET_DIR_RELATIVE_TO_THIS_FILE = '../production_tests'
 MAIN_PY_NAME = 'main.py'
 
 
@@ -66,7 +65,7 @@ def convert_dir_to_py_import(exclude, path):
 
 
 def find_version_file_path(name):
-    scripts_dir = os.path.join(os.path.dirname(__file__), RELATIVE_SCRIPTS_DIR)
+    scripts_dir = os.path.join(os.path.dirname(__file__), TARGET_DIR_RELATIVE_TO_THIS_FILE)
     matching_dirs = search_for_subfolder_by_name(scripts_dir, name)
     if not len(matching_dirs):
         raise ValueError(f'No script subfolders found named \"{args.name}\"')
@@ -95,7 +94,7 @@ if __name__ == '__main__':
         "-o",
         "--out-dir",
         help="Path to output directory",
-        default=RELATIVE_BUILD_DIR,
+        default=DIST_DIR,
         type=str,
     )
     args = parser.parse_args()
@@ -106,42 +105,53 @@ if __name__ == '__main__':
 
     # get the main script path
     main_file_path = os.path.join(os.path.dirname(version_file_path), MAIN_PY_NAME)
+    if not os.path.exists(main_file_path):
+        py_name = f'{args.name}.py'
+        main_file_path = os.path.join(os.path.dirname(version_file_path), py_name)
+        if not os.path.exists(main_file_path):
+            raise FileNotFoundError(
+                'Unable to find a script to build (neither \"{MAIN_PY_NAME}\" nor \"{py_name}\")')
 
-    # check if this current commit is a release or not (is a release if it was tagged)
+    # check if this current commit is a release or not
+    # is a release if this commit was tagged
     repo = git.Repo(search_parent_directories=True)
     expected_tag_name = generate_version_tag(args.name, version)
     is_release = is_commit_a_release(
         repo, expected_tag_name, get_git_commit_hash(repo, length=40))
     if not is_release:
+        # non-releases get the commit hash added to their version
         version = add_commit_hash_to_version(version)
 
-    # generate temporary .version
-    build_dir = os.path.abspath(args.out_dir)
-    tmp_version_file_path = os.path.join(build_dir, VERSION_FILE_NAME)
-    if os.path.exists(build_dir):
-        shutil.rmtree(build_dir)
-    os.mkdir(build_dir)
+    # generate .version file to embed in executable
+    # put it in a dist folder
+    dist_dir = os.path.abspath(args.out_dir)
+    if os.path.exists(dist_dir):
+        shutil.rmtree(dist_dir)
+    os.mkdir(dist_dir)
+    tmp_version_file_path = os.path.join(dist_dir, VERSION_FILE_NAME)
     with open(tmp_version_file_path, 'w') as f:
         f.write(version)
+
+    # build PyInstaller executable, using temporary files
+    build_name = generate_version_tag(args.name, version)
     embedded_data_path = fix_path_for_pyinstaller(tmp_version_file_path)
     if platform.system() == 'Windows':
         embedded_data_path += ';.'
     else:
         embedded_data_path += ':.'
-
-    # build PyInstaller executable, using temporary files
-    build_name = generate_version_tag(args.name, version)
     pyinstaller_args = [
         fix_path_for_pyinstaller(main_file_path),
         '--clean', '--onefile', '-y',
         '--name', build_name.replace('.', '-'),
         '--add-data', embedded_data_path
     ]
-    with open(os.path.join(build_dir, f'build_{build_name}.sh'), 'w') as f:
+    # save the generated pyinstaller command, to help with debugging
+    with open(os.path.join(dist_dir, f'build_{build_name}.sh'), 'w') as f:
         f.write('pyinstaller ' + ' '.join(pyinstaller_args))
     build_exe(pyinstaller_args)
     # move the spec file to the ./build dir
     for f in os.listdir('.'):
         if os.path.isfile(f) and f.split('.')[-1] == 'spec':
-            os.rename(f, os.path.join(build_dir, f))
+            os.rename(f, os.path.join(dist_dir, f))
+
     print(f'\nDone building {build_name}')
